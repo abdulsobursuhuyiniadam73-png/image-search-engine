0d96be2e79deed07668e4d8f3fdba33fe822901f
abdulsobursuhuyiniadam73-png <abdulsobursuhuyiniadam73@gmail.com>
2026-03-17 04:18:22 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const accesskey = "f_qLlxzeEDP6snBoyk6leBDrY8scHwnziPYjSw1JVzk";
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.value;
+    if(!keyword) return;
+
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesskey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    if(page === 1){
+        searchResult.innerHTML = "";
+    }
+
+    results.forEach((result) =>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    });
+
+    showMoreBtn.style.display = "block";
+}
+
+searchForm.addEventListener("submit",(e) =>{
+    e.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMoreBtn.addEventListener("click",() => {
+    page++;
+    searchImages();
+});
